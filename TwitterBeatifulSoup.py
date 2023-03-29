0d89cf8f2c913ace4bb248eb21ac2088bf57c322
memirutku <mustafa.emir@marun.edu.tr>
2023-03-30 01:21:31 +0300

    Update TwitterBeatifulSoup.py

--- TwitterBeatifulSoup.py
+++ TwitterBeatifulSoup.py
@@ -13,15 +13,27 @@ import csv
 import pandas as pd
 
 #google yi açma kısmı
-drive_path="C:\Program Files\Google\Chrome\Application\chrome.exe"
+drive_path="C:\Amazon Games\chromedriver.exe"#chrome driver olmak zorunda o da ekstra indiriliyor
 browser=webdriver.Chrome(drive_path)
-browser.get("https://www.google.com/search?q=tutkumuz+f1&oq=tut&aqs=chrome.0.69i59j69i57j0i131i433i512j69i61j69i60j69i61l2j69i65.1665j0j7&sourceid=chrome&ie=UTF-8")#sorun burada.get çalışmıyor
-"""
-yazi_girisi=browser.find_element(By.ID,"input")#selenium da classlar arasında boşluk var ise nokta koymak gerekiyor #browserden arama kutucuğunu seçme
+browser.get("https://www.google.com/")
+
+
+yazi_girisi=browser.find_element(By.CLASS_NAME,"gLFyf")#selenium da classlar arasında boşluk var ise nokta koymak gerekiyor #browserden arama kutucuğunu seçme
 
 yazi_girisi.send_keys("tutkumuz f1 twitter")#yazı girişi
 time.sleep(2)#yazı girişinden sonra uyutuyor
 yazi_girisi.send_keys(Keys.ENTER)
-"""
+
 tıkla=browser.find_element(By.CLASS_NAME,"haz7je")
 tıkla.click()
+
+
+
+
+
+
+
+
+time.sleep(9999)
+
+#time sleep olayı ney onu sor
